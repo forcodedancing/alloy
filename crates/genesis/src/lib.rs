a527ed073f99caec8780e79b543c1d8eaba3667e
forcodedancing <just.haha.it@gmail.com>
2024-04-25 16:07:54 +0800

    feat: add parlia config for genesis

--- crates/genesis/src/lib.rs
+++ crates/genesis/src/lib.rs
@@ -422,6 +422,10 @@ pub struct ChainConfig {
     /// Clique parameters.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub clique: Option<CliqueConfig>,
+
+    /// Parlia parameters.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub parlia: Option<ParliaConfig>,
 }
 
 impl ChainConfig {
@@ -528,6 +532,18 @@ pub struct EthashConfig {}
 /// Consensus configuration for Clique.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct CliqueConfig {
+    /// Number of seconds between blocks to enforce.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub period: Option<u64>,
+
+    /// Epoch length to reset votes and checkpoints.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub epoch: Option<u64>,
+}
+
+/// Consensus configuration for Parlia.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct ParliaConfig {
     /// Number of seconds between blocks to enforce.
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub period: Option<u64>,
